7c36fb335b49558424980ac2cc64f19b14381c68
froghouse <tomas.landberg@gmail.com>
2015-07-20 18:18:57 +0200

    Create cache.class.php
    The cache class makes sure we don't request more from the API than we need.

---
+++ cache.class.php
@@ -0,0 +1,50 @@
+<?php
+require('file.class.php');
+
+class Cache
+{
+	private $cache;
+	private $fetch;
+	private $nextrun;
+	private static $next;
+	
+	public function __construct($fetch, $cache)
+	{
+		$opts = array(
+		  'ssl'=>array(
+			'method'=>"GET",
+			'header'=>"User-Agent: http://redveng.com/ Maintainer: Tomas Landberg Email: tomas.landberg@gmail.com\r\nAccept-Encoding: gzip"
+		  )
+		);
+		
+		self::$next = "next_run";
+		
+		$this->fetch = new RemoteFile($fetch, $opts);
+		$this->cache = new File($cache);
+		$this->nextrun = new File(self::$next);
+	}
+	
+	private function getNextRun()
+	{
+		return $this->nextrun->get();
+	}
+	
+	private function setNextRun($nr)
+	{
+		$this->nextrun->set($nr);
+	}
+	
+	public function update()
+	{
+		if(($this->getNextRun() - time()) < 0)
+		{
+			$this->cache->set($this->fetch->get());
+			$this->setNextRun(strtotime($this->fetch->headers()['Expires']));
+		}
+	}
+	
+	public function get()
+	{
+		return $this->cache->get();
+	}
+}
