97e89e9723b8bbd167e2420301e9a9dfba1c042c
froghouse <tomas.landberg@gmail.com>
2015-07-20 18:22:36 +0200

    Create zkill.class.php
    The zKill class is responsible for calling update on the cache object and to expose its contents.

---
+++ zkill.class.php
@@ -0,0 +1,25 @@
+<?php
+require('cache.class.php');
+require('urlbuilder.class.php');
+
+class zKill
+{
+	public static $corpID;
+	public static $allianceID;
+	
+	private $cache;
+	
+	public function __construct($corpID, $allianceID)
+	{
+		self::$corpID = $corpID;
+		self::$allianceID = $allianceID;
+		$url = new URLBuilder("https", "zkillboard.com", array("api", "kill", "corporationID", $corpID));
+		$this->cache = new Cache($url->get(), "zKill.json");
+		$this->cache->update();
+	}
+	
+	public function get()
+	{
+		return json_decode($this->cache->get());
+	}
+}
