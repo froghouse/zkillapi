da747dcf2ad43ef39e9be239b9e970df749d498f
froghouse <tomas.landberg@gmail.com>
2015-07-20 18:20:02 +0200

    Create urlbuilder.class.php
    URLBuilder is a helper class to build URLs and make sure they are formated correctly.

---
+++ urlbuilder.class.php
@@ -0,0 +1,38 @@
+<?php
+class URLBuilder
+{
+	private $proto;
+	private $url;
+	private $path;
+	
+	public function __construct($pr, $ur, $pa = array())
+	{
+		$this->proto = $pr;
+		$this->url = $ur;
+		$this->path = $pa;
+	}
+	
+	private function buildPath()
+	{
+		$ret = "";
+		
+		if(is_array($this->path))
+		{
+			foreach($this->path as $pth)
+			{
+				$ret .= "/" . $pth;
+			}
+		}
+		else
+		{
+			$ret .= "/" . $this->path;
+		}
+		
+		return $ret;
+	}
+	
+	public function get()
+	{
+		return $this->proto . "://" . $this->url . $this->buildPath();
+	}
+}
